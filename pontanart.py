0f33af6e9d4429ba97a8e4b8d4b6dd58513c3ca9
Pontana22 <pontusmartinsson@gmail.com>
2025-02-12 22:55:41 +0100

    Move conversion main function to separate file

--- pontanart.py
+++ pontanart.py
@@ -1,9 +1,7 @@
 import os
 from lib.parse_arguments import parse_arguments
 from lib.userinput import process_arguments
-from lib.analyze import analyze
-from lib.image_conversion import get_min_max, convert_image
-from lib.login import login
+from lib.convert import convert
 
 if __name__ == '__main__':
     
@@ -14,17 +12,5 @@ if __name__ == '__main__':
     if arguments.command == 'preset':
         exit(f'preset {arguments.name} successfully created')
     elif arguments.command == 'convert':
-        characters_sorted = analyze(options['characters'], options['analysis_resolution'], options['analysis_font'])
-        
-        if options['mode'] == 'full':
-            min_val, max_val = get_min_max(options['img'], options['row_length'])
-            content = convert_image(options['img'], characters_sorted, options['row_length'], min_val, max_val)
-        elif options['mode'] == 'map':
-            content = convert_image(options['img'], characters_sorted, 0, 255)
-        
-        try:
-            credentials = login(root_dir)
-        except FileNotFoundError:
-            exit('credentials.json not found; see README.md for more info')
-        
+        convert(options, root_dir)
         
