194332b8d4b89457e845532b1c9259393aa05d66
Pontana22 <pontusmartinsson@gmail.com>
2025-02-13 19:36:22 +0100

    Change the output of convert_image

--- lib/image_conversion.py
+++ lib/image_conversion.py
@@ -3,18 +3,17 @@ import numpy as np
 
 def convert_image(image_path: str, characters: str, row_length: int, min: int, max: int):
     image = open_resize(image_path, row_length)
-    output = []
+    output = ''
     input_range = max - min
     output_range = (len(characters) - 1)
     
     for row in image:
-        character_row = ''
         
         for val in row:
             character_index = int(round(((val - min) / input_range) * output_range))
-            character_row += characters[character_index]
+            output += characters[character_index]
 
-        output.append(character_row + '\n')
+        output += '\n'
 
     return(output)
 
